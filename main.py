dc314072b620f85878b78f8e1db4f854d350f0c7
Aran Sena <aransena@Arans-MacBook-Pro-2.local>
2021-07-26 15:29:20 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Basic flask + p5.js server, using getting started examples from p5.js and flask
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(name=None):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/sketch.js
@@ -0,0 +1,41 @@
+let r, g, b;
+let w, h;
+let x, y, rad;
+
+function setup() {
+  frameRate(30);
+  h = windowHeight
+  w = windowWidth
+  createCanvas(w, h);
+  r = random(255);
+  g = random(255);
+  b = random(255);
+}
+
+function draw() {
+  background(127);
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  rad = 100*sin(frameCount/30)
+  x = w/2;
+  y = h/2;
+  ellipse(x, y, rad, rad);
+  let w_new = windowWidth
+  let h_new = windowHeight
+  if(w_new != w || h_new != h){
+    w = w_new;
+    h = h_new;
+    resizeCanvas(w, h);
+  }
+
+}
+
+function mousePressed() {
+  let d = dist(mouseX, mouseY, x, y);
+  if (d < rad) {
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
+}
