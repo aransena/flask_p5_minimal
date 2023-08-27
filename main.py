e24bde8f0b62033d86724e762232956f80f6a599
Aran Sena <aransena@gmail.com>
2023-08-27 01:45:15 +0100

    Websocket backend for transferring data to/from p5.js frontend. Includes example of persistent state sharing across connected clients

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ def home(name=None):
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
+
+
--- static/sketch.js
+++ static/sketch.js
@@ -1,9 +1,21 @@
 let r, g, b;
 let w, h;
 let x, y, radius;
+let counter;
+let message;
+function messageReceived(msg) {
+  message = msg;
+  console.log("Received: " + msg.data);
+}
 
 function setup() {
+  message = null;
 
+  connectWebsocket("ws://127.0.0.1:8765");
+  sendMessage({
+    "msg": "Setup success"
+  });
+  counter = 0;
   frameRate(120);
   h = windowHeight
   w = windowWidth
@@ -16,13 +28,36 @@ function setup() {
 }
 
 function draw() {
+  try {
+    sendMessage({
+      "data": counter,
+      "msg": "Counter value"
+    });
+  } catch (error) {
+    connectWebsocket("ws://127.0.0.1:8765");
+  }
+  
+  counter += 0.01;
   background(127);
+  if(message == null){
   strokeWeight(2);
+  }
+  else{
+  strokeWeight(2);
+  }
   stroke(r, g, b);
   fill(r, g, b, 127);
-  radius = 200+100*sin(frameCount/30)
+  radius = 100;//200+100*sin(frameCount/30)
   x = w/2;
-  y = h/2;
+  if(message != null){
+    x = message["data"]%w
+    y = h/2 + sin(message["data"]*0.01)*radius;
+  }
+  else{
+    x = w/2;
+    y = h/2;
+  }
+  
   ellipse(x, y, radius, radius);
   let w_new = windowWidth
   let h_new = windowHeight
