f5922707c68207764dd45dfb7b01b9693b2cf27a
Aran Sena <aransena@gmail.com>
2023-08-28 01:16:26 +0100

    threaded example for multiple connections sharing and editing data with the websocket server

--- main.py
+++ main.py
@@ -1,15 +1,135 @@
-# Basic flask + p5.js server, using getting started examples from p5.js and flask
+#!/usr/bin/env python
 
-from flask import Flask, render_template
+import asyncio
+from websockets.server import serve
+import threading
+import json
+import math
+from flask import Flask, render_template, request
+import time
 
+class WebsocketServer:
+    def __init__(self) -> None:
+        self.tick = 0.
+        self.state_x = 0.
+        self.state_y = 0.
+        self.target_x = 0.
+        self.target_y = 0.
+
+        self.tick_in = 0.
+        self.tick_in_newest = 0.
+        self.state_x_in = 0.
+        self.state_y_in = 0.
+        self.target_x_in = 0.
+        self.target_y_in = 0.
+
+        self.state = None
+        self.ws = None
+        self.port = None
+        self.websocket_thread = threading.Thread(target=self.websocket_thread)
+        self.state_thread = threading.Thread(target=self.state_thread)
+        self.lock = threading.RLock()
+        self.tick_rate = 0.0001 # for example application
+        self.run()
+    
+    def run(self):
+        self.websocket_thread.start()
+        self.state_thread.start()
+
+    def join(self):
+        self.state_thread.join()
+        self.websocket_thread.join()        
+        
+
+    def websocket_thread(self):
+        asyncio.run(self.start_websocket())
+
+    def state_thread(self):
+        self.state_update()
+
+    def update_state(self, curr_tick):
+        delta = 0.01
+        self.state_x = (self.target_x - self.state_x)*delta + self.state_x
+        self.state_y = (self.target_y - self.state_y)*delta + self.state_y
+
+    async def process_message(self, websocket):
+        async for message in websocket:
+            msg_in = json.loads(message)
+            try:
+                self.tick_in = msg_in["tick"]
+                self.state_x_in = msg_in["state_x"]
+                self.state_y_in = msg_in["state_y"]
+                self.target_x_in = msg_in["target_x"]
+                self.target_y_in = msg_in["target_y"]
+            except:
+                pass
+            curr_tick = self.tick
+            if curr_tick > self.tick_in_newest:
+                self.target_x = self.target_x_in
+                self.target_y = self.target_y_in
+                self.update_state(curr_tick)
+                self.tick_in_newest = self.tick
+            
+
+            msg_out = {}            
+
+            self.update_state
+            
+            msg_out["tick"] = self.tick
+            msg_out["state_x"] = self.state_x
+            msg_out["state_y"] = self.state_y
+            msg_out["target_x"] = self.target_x
+            msg_out["target_y"] = self.target_y
+            await websocket.send(json.dumps(msg_out))
+    
+    async def start_websocket(self):
+        print("Websocket backend waiting")
+        with self.lock:
+            for i in range(8001,30000):
+                try:
+                    async with serve(self.process_message, "0.0.0.0", i):
+                        self.port = i
+                        print(f"Starting server messaging using port {self.port}")
+                        await asyncio.Future()  # run forever
+                except Exception as e:
+                    print(e)
+                    pass
+            
+        print("Websocket server ended")
+
+    def state_update(self):
+        try:
+            print("Ticking example")
+            while True:
+                self.tick += 0.1
+                time.sleep(self.tick_rate)
+                # print(f"{self.tick:0.3f}", end="\r")
+        except KeyboardInterrupt as e:
+            pass
 
 app = Flask(__name__)
 
 @app.route('/')
 def home(name=None):
-    return render_template('index.html', name=name)
+        global port
+        hostname = request.headers.get('Host').split(":")[0]
+        print(f"HOST: {hostname}")
+        return render_template('index.html', name=name, hostname=hostname, port=port)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+if __name__=="__main__":
+    global port
+    port = None
+    print("Starting websocket backend")
+    ws_server = WebsocketServer()
 
+    while port is None:
+        port = ws_server.port
+    print(f"USING: {port}")
 
+    
+    app.run(debug=True, host="0.0.0.0", port=8000)
+    
+   
+
+    ws_server.join()
+    print("Exit")
--- static/sketch.js
+++ static/sketch.js
@@ -3,22 +3,51 @@ let w, h;
 let x, y, radius;
 let counter;
 let message;
+let port;
+let hostname;
+let tick;
+let ws_url;
+let target_x;
+let target_y;
+let state_x;
+let state_y;
+
+
 function messageReceived(msg) {
+  // Callback function that receives data from remote websocket server
   message = msg;
-  console.log("Received: " + msg.data);
+  let new_tick = message['tick'];
+  if(new_tick > tick){
+    tick = new_tick;
+    state_x = message['state_x']*w;
+    state_y = message['state_y']*h;
+    target_x = message['target_x']*w;
+    target_y = message['target_y']*h;
+  }
 }
 
 function setup() {
+  
   message = null;
-
-  connectWebsocket("ws://127.0.0.1:8765");
+  tick = 0.0;
+  port = document.getElementsByName('userscript')[0].getAttribute('port_value');
+  
+  hostname = document.getElementsByName('userscript')[0].getAttribute('host_name');
+  ws_url = "ws://"+hostname+":"+port;
+  console.log(port);
+  connectWebsocket(ws_url);
   sendMessage({
     "msg": "Setup success"
   });
   counter = 0;
   frameRate(120);
-  h = windowHeight
-  w = windowWidth
+  h = windowHeight;
+  w = windowWidth;
+  state_x = w/2;
+  state_y = h/2;
+  target_x = w/2;
+  target_y = h/2;
+  
   createCanvas(w, h);
   r = random(255);
   g = random(255);
@@ -28,37 +57,41 @@ function setup() {
 }
 
 function draw() {
-  try {
-    sendMessage({
-      "data": counter,
-      "msg": "Counter value"
-    });
-  } catch (error) {
-    connectWebsocket("ws://127.0.0.1:8765");
-  }
-  
-  counter += 0.01;
   background(127);
-  if(message == null){
-  strokeWeight(2);
-  }
-  else{
-  strokeWeight(2);
-  }
+
   stroke(r, g, b);
   fill(r, g, b, 127);
-  radius = 100;//200+100*sin(frameCount/30)
-  x = w/2;
-  if(message != null){
-    x = message["data"]%w
-    y = h/2 + sin(message["data"]*0.01)*radius;
-  }
-  else{
-    x = w/2;
-    y = h/2;
+  let lambda = 0.01;
+  
+  counter += 0.1; // Demo local value to send to remote websocket server
+  
+  // Send data back to remote websocket server
+  try {
+    
+    sendMessage({
+      "data": counter,
+      "msg": "Counter value",
+      "tick": tick,
+    });
+  } catch (error) {
+    connectWebsocket(ws_url);
   }
   
-  ellipse(x, y, radius, radius);
+  // Simple example for this code
+  try {
+    // console.log(state_x);
+    ellipse(
+      state_x, 
+      state_y,
+      30+sin(tick/100.)*10,
+      30+sin(tick/100.)*10
+    );
+    
+  } catch (error) {
+    
+  }
+
+  // Check for window resize  
   let w_new = windowWidth
   let h_new = windowHeight
   if(w_new != w || h_new != h){
@@ -66,14 +99,27 @@ function draw() {
     h = h_new;
     resizeCanvas(w, h);
   }
-
+    
 }
 
+
+
 function mousePressed() {
-  let d = dist(mouseX, mouseY, x, y);
-  if (d < radius) {
-    r = random(255);
-    g = random(255);
-    b = random(255);
+  target_x = mouseX;
+  target_y = mouseY;
+  // Send data back to remote websocket server
+  try {
+    sendMessage({
+      "data": counter,
+      "msg": "Counter value",
+      "tick": tick,
+      "state_x": state_x/w,
+      "state_y": state_y/h,
+      "target_x": target_x/w,
+      "target_y": target_y/h
+    });
+  } catch (error) {
+    connectWebsocket(ws_url);
   }
+
 }
